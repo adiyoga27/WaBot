34ad9dcaf0cb6b159bf0fc3d7e6457f17d9c51d3
Adiyoga <adiyoga27new@gmail.com>
2023-06-16 13:29:18 +0800

    App

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const socketIO = require('socket.io');
 const bodyParser = require('body-parser') 
 const process = require('process');
 const http = require('http');
+const fs = require('fs');
+
 const {infoLog, emergecyLog} = require('./Services/telegram');
 const { info } = require('console');
 require('dotenv').config()
@@ -14,13 +16,54 @@ process.setMaxListeners(0);
 const port = process.env.APP_PORT || 2929;
 
 const app = express();
-const server = http.createServer(app);
+let server;
+if (process.env.APP_PROTOCOL === "https") {
+  // Certificate
+  const credentials = {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/wabot.galkasoft.id/privkey.pem",
+      "utf8"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/wabot.galkasoft.id/cert.pem",
+      "utf8"
+    ),
+    ca: fs.readFileSync(
+      "/etc/letsencrypt/live/wabot.galkasoft.id/chain.pem",
+      "utf8"
+    ),
+  };
+  let http = require("https");
+
+  server = http.createServer(credentials, app);
+} else {
+  let http = require("http");
+  server = http.createServer(app);
+}
+// const server = http.createServer(app);
+
 const io = socketIO(server, {
     allowEIO3: true // false by default
 });
 gettingStarted(io);
 app.set('socketio', io);
-
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost",
+      "http://127.0.0.1",
+      "http://localhost:7991",
+      "http://127.0.0.1:7991",
+      "http://wabot.galkasoft.id:7991",
+      "https://wabot.galkasoft.id:7991",
+    ],
+  }),
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+  bodyParser.json()
+);
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
